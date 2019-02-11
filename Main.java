77934081de5eb3e20cdad95b10a3306fa69a3221
Jesus Salvador Cortes Gutierrez <31747651+noxtla@users.noreply.github.com>
2019-02-11 13:28:19 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,30 @@
+package Condicionales;
 
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+	int band = 1;
+	int numero, random=0;
+	
+
+
+	 do {	
+		 
+		 numero = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el numero "));
+
+		 	
+		    random += numero;
+		 	
+		 	if (numero == 0) {
+				band = 0;
+			}
+				
+				
+		} while (band != 0);
+		
+		
+		JOptionPane.showMessageDialog(null, "La suma total es " + random);
+	}
+}
